fa127923a659592acfff4a5588f89c1bf659ad6b
Patrik Wibron <patrik@wibron.nu>
2015-06-28 13:21:27 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,11 @@
+# Systembolaget APK Chrome Extension
+
+Print the [APK](https://sv.wikipedia.org/wiki/Alkohol_per_krona) for the current selected beverage on Systembolaget.se.
+
+## Development 
+
+* Go to `chrome://extensions` and choose `Load unpacked extension...`
+
+* Make sure you have the [Livereload Extension](https://www.google.se/url?sa=t&rct=j&q=&esrc=s&source=web&cd=1&cad=rja&uact=8&ved=0CCEQFjAA&url=https%3A%2F%2Fchrome.google.com%2Fwebstore%2Fdetail%2Flivereload%2Fjnihajbhpnppcggbcgedagnkighmdlei&ei=-diPVe2KK-rWygOY57PgCg&usg=AFQjCNHbu-zBNeWaEOiLMxwl6wGfZGkpzQ&bvm=bv.96783405,d.bGQ) installed to make the development process easier
+
+* Run `grunt debug` 
---
+++ app/scripts/contentscript.js
@@ -0,0 +1,52 @@
+(function () {
+  'use strict';
+
+  var detailsList = document.querySelector('.details-list');
+  var productHeader = document.querySelector('.product-header');
+  var rightCol = document.querySelector('.product-header .right-col');
+
+  if (!productHeader) {
+    return;
+  }
+
+  var formatAlcohol = function (string) {
+    return string.replace(',', '.').replace('%', '').trim();
+  };
+
+  var formatVolume = function (volume) {
+    return /([0-9]*)\&nbsp;ml$/.exec(volume)[1];
+  };
+
+  var formatPrice = function (price) {
+    return price.replace(':', '.').replace('-', '0');
+  };
+
+  var getApk = function (price, alcohol, volume) {
+    var apk = ((alcohol / 100) * volume) / price;
+    apk = parseFloat(apk.toFixed(2));
+
+    return apk;
+  };
+
+  var updateApkOnProductPage = function () {
+    var price = parseFloat(formatPrice(productHeader.querySelector('.price').innerHTML));
+    var alcohol = parseFloat(formatAlcohol(detailsList.querySelector('li:nth-child(2) p').innerHTML));
+    var volume = parseFloat(formatVolume(productHeader.querySelector('.packaging').innerHTML));
+
+    console.log(price, alcohol, volume);
+
+    var apkListItem = document.createElement('li');
+    apkListItem.innerHTML = '<h3>APK</h3><p>' + getApk(price, alcohol, volume) + '</p>';
+
+    detailsList.querySelector('ul').appendChild(apkListItem);
+
+    var apkDetailItem = document.createElement('li');
+    apkDetailItem.innerHTML = getApk(price, alcohol, volume) + ' APK';
+    rightCol.appendChild(apkDetailItem);
+  };
+
+  if (productHeader) {
+    updateApkOnProductPage();
+  }
+
+}());
